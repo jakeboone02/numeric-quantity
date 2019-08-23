f1bfee3da5b9fa0f94bb0a1f328e6677ee2d27de
Jake Boone <jakeboone02@gmail.com>
2019-08-23 15:06:58 -0700

    Ignore dist folder

--- dist/numeric-quantity.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Converts a string to a number.  The string can include mixed numbers
- * or vulgar fractions.
- */
-declare function numericQuantity(qty: string): number;
-export default numericQuantity;
