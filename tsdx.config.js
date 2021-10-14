1d76f17624f8a37979d394799a95f9cd0628ca0a
Jake Boone <jakeboone02@gmail.com>
2021-10-14 12:06:47 -0700

    Update dependencies

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,7 +1,17 @@
+const replace = require('@rollup/plugin-replace');
+
 module.exports = {
   rollup(config, options) {
     config.output.name = 'numericQuantity';
     config.output.exports = 'default';
+    config.plugins = config.plugins.map(p =>
+      p.name === 'replace'
+        ? replace({
+            'process.env.NODE_ENV': JSON.stringify(options.env),
+            preventAssignment: true,
+          })
+        : p
+    );
     return config;
   },
 };
