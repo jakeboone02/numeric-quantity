3a86918dddf5ed97e713455cff6330db6df86458
Jake Boone <jakeboone02@gmail.com>
2022-04-15 15:19:44 -0700

    Migrate from tsdx to vite

---
+++ src/dev.ts
@@ -0,0 +1,5 @@
+import numericQuantity from '.';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerText = JSON.stringify(numericQuantity('1.5'));
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      fileName: format => `numeric-quantity.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+      name: 'numericQuantity',
+    },
+    sourcemap: true,
+  },
+});
