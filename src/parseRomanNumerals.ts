8ae0701de53504af704b7888dd9e0d3faae405b2
Jake Boone <jakeboone02@gmail.com>
2023-06-13 17:28:10 -0700

    Add JSDoc comment to `parseRomanNumerals`

--- src/parseRomanNumerals.ts
+++ src/parseRomanNumerals.ts
@@ -8,13 +8,22 @@ import {
 // Just a shorthand type alias
 type RNV = keyof typeof romanNumeralValues;
 
+/**
+ * Converts a string of Roman numerals to a number, like `parseInt`
+ * for Roman numerals. Uses modern, strict rules (only 1 to 3999).
+ *
+ * The string can include ASCII representations of Roman numerals
+ * or Unicode Roman numeral code points (`U+2160` through `U+217F`).
+ */
 export const parseRomanNumerals = (romanNumerals: string) => {
   const normalized = `${romanNumerals}`
+    // Convert Unicode Roman numerals to ASCII
     .replace(
       romanNumeralUnicodeRegex,
       (_m, rn: keyof typeof romanNumeralUnicodeToAsciiMap) =>
         romanNumeralUnicodeToAsciiMap[rn]
     )
+    // Normalize to uppercase (more common for Roman numerals)
     .toUpperCase();
 
   const regexResult = romanNumeralRegex.exec(normalized);
@@ -26,9 +35,9 @@ export const parseRomanNumerals = (romanNumerals: string) => {
   const [, thousands, hundreds, tens, ones] = regexResult;
 
   return (
-    (romanNumeralValues[thousands as RNV] || 0) +
-    (romanNumeralValues[hundreds as RNV] || 0) +
-    (romanNumeralValues[tens as RNV] || 0) +
-    (romanNumeralValues[ones as RNV] || 0)
+    (romanNumeralValues[thousands as RNV] ?? 0) +
+    (romanNumeralValues[hundreds as RNV] ?? 0) +
+    (romanNumeralValues[tens as RNV] ?? 0) +
+    (romanNumeralValues[ones as RNV] ?? 0)
   );
 };
